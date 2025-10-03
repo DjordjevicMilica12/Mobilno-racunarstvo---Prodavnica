e4f66f8aeb96c4a57786d972dbcfd82968a5eacd
Milica Djordjevic <djordjevicmilica12@gmail.com>
2025-10-03 11:05:10 +0200

    Dodat .net servis

--- obj/Debug/net8.0/Prodavnica-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Prodavnica-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prodavnica-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1ddbb24f2311ec786963e868793e52134ad34e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prodavnica-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prodavnica-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
